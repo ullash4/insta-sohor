44c19d8555f55412ecb0f593715e2d168cf03cd3
ullash4 <programmerullash@gmail.com>
2022-03-11 00:34:58 +0600

    caption bug fixed

--- js/app.js
+++ js/app.js
@@ -27,7 +27,14 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  console.log(text);
+  // let overView;
+  if(text.length > 30){
+    return text.slice(0, 30)+ "<span class='fw-bold'>... read more</span>";
+  }else{
+    return text;
+  }
+    // return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
